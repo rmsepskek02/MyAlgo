c35798485188aaa71d6913df7c7ed9f53da92319
rmsepskek02 <rmsepskek02@gmail.com>
2024-11-22 13:14:12 +0900

    1122 강의
    - List Node

--- ArrayTest/ArrayTest.cpp
+++ ArrayTest/ArrayTest.cpp
@@ -3,24 +3,123 @@
 
 #include <iostream>
 #include <array>
+#include <vector>
 using namespace std;
 
-//동적 메모리 할당 예제
+//2행 3열 원소를 출력하라
+//c++ vector 2차원 배열 활용
 int main() {
-	
-	//동적 메모리 할당
-	int* ptr = new int[3] {};
-	ptr[0] = 10;
-	ptr[1] = 20;
+	//2차원 배열 선언 및 초기화
+	vector<vector<int>> mat1(2, vector<int>(3, 0));
 
-	for (int i = 0; i < 3; i++) {
-		cout << ptr[i] << endl;
+	vector<vector<int>> mat2{
+		{1,2,3},
+		{4,5,6}
+	};
+
+	//출력:
+	for (int x = 0; x < mat2.size(); x++) {
+		for (int y = 0; y < mat2[x].size(); y++) {
+			cout << mat2[x][y] << " ";
+		}
+		cout << endl;
 	}
-
-	//동적 메모리 해제
-	delete[] ptr;
-	ptr = nullptr;
+	//output 
+	//1 2 3 
+	//4 5 6
 }
+
+//2행 3열 원소의 합을 구하라
+//c언어 2차원 배열
+//int main() {
+//	int mat[2][3] = {
+//		{1,2,3},
+//		{4,5,6}
+//	};
+//	int sum = 0;
+//	for (int r = 0; r < 2; r++) {
+//		for (int c = 0; c < 3; c++) {
+//			sum += mat[r][c];
+//		}
+//	}
+//
+//	cout << "SUM = " << sum << endl;
+//}
+
+//가변형 배열 vector 활용
+//int main() {
+//	vector<int> v1;
+//	v1.push_back(10);
+//	v1.push_back(20);
+//	v1.push_back(30);
+//
+//	vector<int> v2(10);		//0으로 초기화된 10개의 방 생성
+//	vector<int> v3(10, 1);	//1로 초기화된 10개의 방 생성
+//	vector<int> v4{ 10,20,30,40,50 };
+//
+//	vector<int> v5(v4);
+//	vector<int> v6(v4.begin(), v4.begin() + 3 );
+//
+//	for (int i = 0; i < v6.size(); i++) {
+//		cout << v6[i] << endl;
+//	}
+//}
+
+//동적 메모리를 관리하는 클래스
+//template <typename T> 
+//class DynamicArray {
+//private:
+//	unsigned int sz;
+//	T* arr;
+//
+//public:
+//	//생성자: 매개변수를 크기를 입력받아 동적 메모리 할당
+//	DynamicArray(int n) : sz(n)
+//	{
+//		arr = new T[sz];
+//	}
+//
+//	//소멸자 : 할당된 메모리를 해제
+//	~DynamicArray() 
+//	{
+//		delete[] arr;
+//		cout << "동적메모리 해제" << endl;
+//	}
+//
+//	unsigned int size() {
+//		return sz;
+//	}
+//
+//	T& operator[] (const int i) { return arr[i]; }
+//};
+//
+//int main() {
+//	DynamicArray<int> da(5);
+//	da[0] = 10;
+//	da[1] = 20;
+//	da[2] = 30;
+//
+//	for (int i = 0; i < da.size(); i++) {
+//		cout << da[i] << endl;
+//	}
+//}
+
+//동적 메모리 할당 예제
+//int main() {
+//	
+//	//동적 메모리 할당
+//	int* ptr = new int[3] {};
+//	ptr[0] = 10;
+//	ptr[1] = 20;
+//
+//	for (int i = 0; i < 3; i++) {
+//		cout << ptr[i] << endl;
+//	}
+//
+//	//동적 메모리 해제
+//	delete[] ptr;
+//	ptr = nullptr;
+//}
 
 //C++언어 배열 구현 - 점수들의 평균을 구하라
 //int main() {
---
+++ ListTemp/ListTemp.cpp
@@ -0,0 +1,95 @@
+﻿// ListTemp.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+using namespace std;
+struct Node {
+    int data;       // 저장할 데이터
+    Node* next;     // 다음에 올 노드
+
+};
+
+class LinkedList {
+private:
+    Node* head;     // 맨 앞에 있는 노드
+
+public:
+    // 생성자
+    LinkedList() : head(NULL) {
+
+    };
+
+    //소멸자
+    ~LinkedList() {
+        //생성한 node의 메모리 해제
+        while (!empty()) {
+            pop_front();
+        }
+    }
+
+    //매개변수를 데이터(value)를 입력받아 리스트 맨 앞에 삽입하기
+    void push_front(int value) {
+        //데이터(value)를 가진 노드 생성
+        Node* new_node = new Node{ value, NULL };
+
+        if (head != NULL) {
+            new_node->next = head;
+        }
+        head = new_node;
+    }
+    //리스트 맨 앞에 있는 데이터 삭제하기
+    void pop_front() {
+        if (head == NULL) {
+            return;
+        }
+        Node* first = head;
+        head = head->next;
+        delete first;
+    }
+    //리스트의 모든 노드 데이터 출력
+    void print_all() {
+        Node* curr = head;
+
+        //curr가 null이 될때까지 반복
+        while (curr != NULL) {
+            //현재 curr 노드의 data 출력
+            cout << curr->data << ", ";
+
+            //curr 노드를 다음 노드로 이동한다
+            curr = curr->next;
+        }
+        cout << endl;
+    }
+
+    //데이터 유무
+    bool empty() {
+        return head == NULL;
+    }
+};
+
+
+
+int main()
+{
+    LinkedList ll;
+    ll.push_front(10);
+    ll.push_front(20);
+    ll.push_front(30);
+    ll.print_all();
+    ll.pop_front();
+    ll.print_all();
+    ll.push_front(40);
+    ll.print_all();
+  
+}
+
+// 프로그램 실행: <Ctrl+F5> 또는 [디버그] > [디버깅하지 않고 시작] 메뉴
+// 프로그램 디버그: <F5> 키 또는 [디버그] > [디버깅 시작] 메뉴
+
+// 시작을 위한 팁: 
+//   1. [솔루션 탐색기] 창을 사용하여 파일을 추가/관리합니다.
+//   2. [팀 탐색기] 창을 사용하여 소스 제어에 연결합니다.
+//   3. [출력] 창을 사용하여 빌드 출력 및 기타 메시지를 확인합니다.
+//   4. [오류 목록] 창을 사용하여 오류를 봅니다.
+//   5. [프로젝트] > [새 항목 추가]로 이동하여 새 코드 파일을 만들거나, [프로젝트] > [기존 항목 추가]로 이동하여 기존 코드 파일을 프로젝트에 추가합니다.
+//   6. 나중에 이 프로젝트를 다시 열려면 [파일] > [열기] > [프로젝트]로 이동하고 .sln 파일을 선택합니다.
